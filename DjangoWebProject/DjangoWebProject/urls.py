be74306d366e533485aa868745b723d15563c3f4
Tobgay, Tashi <0624012@schoolsnet.act.edu.au>
2026-05-23 20:02:42 +1000

    Upgraded

--- DjangoWebProject/DjangoWebProject/urls.py
+++ DjangoWebProject/DjangoWebProject/urls.py
@@ -4,6 +4,7 @@ import HelloDjangoApp.views
 urlpatterns = [
     re_path(r'^$', HelloDjangoApp.views.index, name='index'),
     re_path(r'^home$', HelloDjangoApp.views.index, name='home'),
+    re_path(r'^about$', HelloDjangoApp.views.about, name='about'),
     re_path(r'^generate_outline$', HelloDjangoApp.views.generate_outline, name='generate_outline'),
     re_path(r'^assessments$', HelloDjangoApp.views.assessments, name='assessments'), 
 ]
--- DjangoWebProject/HelloDjangoApp/views.py
+++ DjangoWebProject/HelloDjangoApp/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
 
+def about(request):
+    return render(request, 'HelloDjangoApp/About.html')
+
 def index(request):
     return render(request, 'HelloDjangoApp/Index.html') 
 
@@ -33,4 +36,5 @@ def assessments(request):
     ]
      
     return render(request, 'Assessments.html', {'assessments': assessment_list})
+
 
